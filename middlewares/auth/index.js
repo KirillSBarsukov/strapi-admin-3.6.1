157efc93f4274967d9a4393c685e7ddf54bc52e5
Kirill Barsukov <kirillsbarsukov@gmail.com>
2021-04-29 16:37:35 -0400

    first commit

---
+++ middlewares/auth/index.js
@@ -0,0 +1,62 @@
+'use strict';
+
+
+const {createManually, update} = require('./../../controllers/user');
+
+module.exports = strapi => ({
+  async initialize() {
+    const passportMiddleware = strapi.admin.services.passport.init();
+
+    strapi.app.use(passportMiddleware);
+
+    strapi.app.use(async (ctx, next) => {
+      if (
+        ctx.request.header.authorization &&
+        ctx.request.header.authorization.split(' ')[0] === 'Bearer'
+      ) {
+        const token = ctx.request.header.authorization.split(' ')[1];
+
+        const { payload, isValid } = strapi.admin.services.token.decodeJwtToken(token);
+
+        if (isValid) {
+          // request is made by an admin
+          let admin = await strapi.query('user', 'admin').findOne({ id: payload.id }, ['roles']);
+
+          // create
+          if(admin === null){
+            try {
+              const role = await strapi.admin.services.role.findOne({ name:  payload.role });
+              const hashedPassword = await strapi.admin.services.auth.hashPassword("Demo1234");
+              console.log("hashed pass", hashedPassword)
+              admin = await  createManually(
+                {email:  payload.email,
+                  firstname: payload.firstName,
+                  lastname: payload.lastName,
+                  username: payload.subject,
+                  password: await hashedPassword,
+                  isActive: true,
+                  roles: [role.id]}
+              );
+            } catch (e){
+              admin = await strapi.query('user', 'admin').findOne({ email: payload.email }, ['roles']);
+            }
+          }
+
+          if (!admin || !(admin.isActive === true)) {
+            return ctx.forbidden('Invalid credentials');
+          }
+
+          ctx.state.admin = admin;
+          ctx.state.user = admin;
+          ctx.state.userAbility = await strapi.admin.services.permission.engine.generateUserAbility(
+            admin
+          );
+          ctx.state.isAuthenticatedAdmin = true;
+          return next();
+        }
+      }
+
+      return next();
+    });
+  },
+});
---
+++ services/token.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const crypto = require('crypto');
+const _ = require('lodash');
+const jwt = require('jsonwebtoken');
+const axios = require("axios");
+
+const defaultJwtOptions = { expiresIn: '30d' };
+
+const getTokenOptions = () => {
+  const { options, secret } = strapi.config.get('server.admin.auth', {});
+
+  return {
+    secret,
+    options: _.merge(defaultJwtOptions, options),
+  };
+};
+
+/**
+ * Create a random token
+ * @returns {string}
+ */
+const createToken = () => {
+  return crypto.randomBytes(20).toString('hex');
+};
+
+/**
+ * Creates a JWT token for an administration user
+ * @param {object} user - admin user
+ */
+const createJwtToken = user => {
+  const { options, secret } = getTokenOptions();
+
+  return jwt.sign({ id: user.id, email: user.email  }, secret, options);
+};
+
+/**
+ * Tries to decode a token an return its payload and if it is valid
+ * @param {string} token - a token to decode
+ * @return {Object} decodeInfo - the decoded info
+ */
+const decodeJwtToken = async token => {
+  const { secret } = getTokenOptions();
+
+  try {
+    const payload = jwt.verify(token, secret);
+    return { payload, isValid: true };
+  } catch (err) {
+    const {data} = await axios({
+      method: 'post',
+      url:  `${process.env.EXTERNAL_JWT_VALIDATION}validatejwt.ajx`,
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+
+    if (data.validity) {
+      const payload = jwt.decode(token)
+
+      return { payload: { email: payload.email, iat: 1618327950, exp: 1620919950, role: payload.adminRoleName, firstName: payload.adminFirstName, lastName: payload.adminLastName},  isValid: data.validity };
+    }
+
+    return {payload: null, isValid: false};
+  }
+};
+
+module.exports = {
+  createToken,
+  createJwtToken,
+  getTokenOptions,
+  decodeJwtToken,
+};
